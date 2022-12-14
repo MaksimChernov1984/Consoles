a43ad48fcb06de5d7c78332e08b5fb319dad2dd2
Maksim Chernov <120552544+MaksimChernov1984@users.noreply.github.com>
2022-12-14 10:59:06 +0200

    Add files via upload

---
+++ Spanish numbers.py
@@ -0,0 +1,168 @@
+x = int(input("Напишите любую цифру, а я напишу её на испанском  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("А ещё?  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("Ещё!  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("Мне понравилось, хочу ещё цифру!  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("Ещё одну.  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("И ещё разочек.  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+print()
+
+x = int(input("Теперь точно последний раз, честно.  "))
+if(x) == 1:
+    x = "Uno"
+elif(x) == 2:
+    x = "Dos"
+elif(x) == 3:
+    x = "Tres"
+elif(x) == 4:
+    x = "Quatro"
+elif(x) == 5:
+    x = "Cinco"
+elif(x) == 6:
+    x = "Seis"
+elif(x) == 7:
+    x = "Siete"
+elif(x) == 8:
+    x = "Ocho"
+elif(x) == 9:
+    x = "Nueve"
+else:
+    x = "Cero"
+print(x)
+input()
+
