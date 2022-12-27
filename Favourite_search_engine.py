cdaa11824993e187cacb8007bc86b8f7b0fdc7ef
Maksim Chernov <120552544+MaksimChernov1984@users.noreply.github.com>
2022-12-28 00:01:15 +0200

    Add files via upload

--- Favourite_search_engine.py
+++ Favourite_search_engine.py
@@ -1,86 +1,94 @@
-#код, написанный для изучения библиотеки tkinter
+# код, написанный для изучения библиотеки tkinter
 from tkinter import *
-from tkinter.ttk import Combobox #выпадающий список
-from tkinter import messagebox #всплывающее окно
-from tkinter import ttk #для кнопки Выход
+from tkinter.ttk import Combobox  # выпадающий список
+from tkinter import messagebox  # всплывающее окно
 
-#окно
+
+# окно
 window = Tk()
 window.title('Любимый поисковик')
 window.geometry('600x400')
-window['bg']='#2F4F4F'
+window['bg'] = '#2F4F4F'
 
-#верхний отступ
+# верхний отступ
 lbl = Label(window, text=' ', bg='#2F4F4F')
 lbl.grid(column=1, row=0)
 
-#всплывающее окно
-def clicked():
-    messagebox.askyesno(title='Вопрос', message='Вы любите искать информацию?')
+# всплывающее окно с вопросом
+def clicked_ask():
+    answ = messagebox.askquestion('Вопрос', 'Вы любите искать информацию в интернете?')
+    if answ == 'yes':
+        messagebox.showinfo('Если да', 'Расскажите о своём любимом поисковике!')
+    elif answ == 'no':
+        messagebox.showinfo('Если нет', 'Наверное, Вы чаще пользуетесь книгами!')
+    else:
+        messagebox.showwarning('Ошибка', 'Что-то пошло не так.')
 
-#кнопка Вопрос
-btn_ask = Button(window, text="Вопрос", bg='#CD5C5C', fg='#fff', command=clicked)
+# кнопка Вопрос
+btn_ask = Button(window, text="Вопрос", bg='#CD5C5C', fg='#fff', command=clicked_ask)
 btn_ask.grid(column=1, row=2)
 
-#отступ после кнопки Вопрос
+# отступ после кнопки Вопрос
 lbl = Label(window, text=' ', bg='#2F4F4F')
 lbl.grid(column=1, row=3)
 
-#начало текста
-lbl = Label(window, text='   Назовите любимый поисковик ', font=(16), fg='#fff', bg='#2F4F4F')
+# начало текста
+lbl = Label(window, text='   Назовите любимый поисковик ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=4)
 
-#выпадающий список
+# выпадающий список
 combo = Combobox(window)
 combo['values'] = ('(выберите)', 'Google', 'Яндекс', 'Mail.ru', 'Yahoo!', 'Web.de', 'Рамблер')
 combo.current(0)
 combo.grid(column=2, row=4)
 
-#отступ после выпадающего списка
-lbl = Label(window, text=' ', font=(16), fg='#fff', bg='#2F4F4F')
+# отступ после выпадающего списка
+lbl = Label(window, text=' ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=5)
 
-#надпись
-lbl = Label(window, text='   Назовите причину ', font=(16), fg='#fff', bg='#2F4F4F')
+# надпись
+lbl = Label(window, text='   Назовите причину ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=6)
 
-#ввод текста
+# ввод текста
 txt = Entry(window, width=30)
 txt.grid(column=2, row=6)
 txt.focus()
 
-#отступ после причины
-lbl = Label(window, text=' ', font=(16), fg='#fff', bg='#2F4F4F')
+# отступ после причины
+lbl = Label(window, text=' ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=7)
 
-#надпись сколько баллов
-lbl = Label(window, text='Сколько баллов из 10', font=(16), fg='#fff', bg='#2F4F4F')
+# надпись сколько баллов
+lbl = Label(window, text='Сколько баллов из 10', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=8)
 
-#количество баллов
+# количество баллов
 spin = Spinbox(window, from_=1, to=10, width=5)
 spin.grid(column=2, row=8)
 #spin.current(10)
 
-#отступ после баллов
-lbl = Label(window, text=' ', font=(16), fg='#fff', bg='#2F4F4F')
+# отступ после баллов
+lbl = Label(window, text=' ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=9)
 
-#появляется надпись при клике, и это должно быть перед кнопкой
+# появляется надпись при клике, и это должно быть перед кнопкой
 def clicked():
     res = 'Моя любимая поисковая система - ' + combo.get() + ', \nпотому ' \
         'что ' + txt.get() + '. \nКоличество баллов, которые я бы ему дал - ' + spin.get() + '.'
     lbl.configure(text=res, font='16', fg='#fff', bg='#2F4F4F')
     lbl.grid(columnspan=1+2+3+4, row=12)
 
-#кнопка Проверить
+# кнопка Проверить
 btn_check = Button(window, text='Проверить', bg='#CD5C5C', fg='#fff', command=clicked)
 btn_check.grid(column=1, row=10)
 
-#отступ после кнопки Проверить
-lbl = Label(window, text=' ', font=(16), fg='#fff', bg='#2F4F4F')
+# отступ после кнопки Проверить
+lbl = Label(window, text=' ', font=16, fg='#fff', bg='#2F4F4F')
 lbl.grid(column=1, row=11)
 
-btn_exit = Button(window, text="Выход", command=window.destroy).grid(column=1, row=13)
+# кнопка Выход
+btn_exit = Button(window, text="Выход", command=window.destroy)
+btn_exit.grid(column=1, row=13)
 
 window.mainloop()
