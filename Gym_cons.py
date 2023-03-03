a308d77fac940e1e61df183fbe4ff09a8ebaa864
Maksim Chernov <120552544+MaksimChernov1984@users.noreply.github.com>
2023-03-03 07:06:28 +0200

    Add files via upload

--- Gym_cons.py
+++ Gym_cons.py
@@ -1,4 +1,19 @@
-f = open('Gym.txt', 'a')
-gym = input('Я отжался столько раз:  ')
-f.write(gym+'\n')
+y = input('Выберите:\nстарт/запись/чтение/сумма\n')
+if y == 'старт':
+	f = open('gym.py', 'w')
+	f.write('x = ')
+elif y == 'запись':
+	f = open('gym.py', 'a')
+	gym = int(input('Я отжался столько раз:  '))
+	f.write(str(gym)+'+')
+elif y == 'чтение':
+	f = open('gym.py')
+	for line in f:
+		print(line)
+elif y == 'сумма':
+	f = open('gym.py', 'a')
+	f.write('0')
 f.close()
+if y == 'сумма':
+	from gym import x
+	print(x)
